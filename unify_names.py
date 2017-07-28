5a0c5ab582614d721638d057c46e6a07a2cd72e0
Chao <chaoguo94@gmail.com>
2017-07-28 11:33:17 +0800

    Update unify_names.py

--- unify_names.py
+++ unify_names.py
@@ -1,7 +1,5 @@
 def unify_name(name_list):
     sorted_list = sorted(name_list, key=lambda name: len(name))
     for i in range(0,len(sorted_list)):
-        for j  in range(i+1,len(sorted_list)):
-            if sorted_list[i] in sorted_list[j]:
-                sorted_list[j] = sorted_list[i]
+        sorted_list = [sorted_list[i] for x in sorted_list if sorted_list[i] in x]
     return sorted_list
